911d4e264aa832f6b75358ca28398d1e818d66fb
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-27 19:48:33 -0500

    Create practice server.js file

---
+++ Week_6/ejs_practice/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const PORT = 8080
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+// about page
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+app.listen(PORT)
+console.log(`Server is listening on port ${PORT}`);
