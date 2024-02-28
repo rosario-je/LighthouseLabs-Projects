37f7620566e4ca616065cb9776637c7301aa1e77
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-02-27 19:53:45 -0500

    Add array definition for mascots

--- Week_6/ejs_practice/server.js
+++ Week_6/ejs_practice/server.js
@@ -9,7 +9,17 @@ app.set('view engine', 'ejs');
 
 // index page
 app.get('/', function(req, res) {
-  res.render('pages/index');
+  var mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012},
+    { name: 'Tux', organization: "Linux", birth_year: 1996},
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013}
+  ];
+  var tagline = "No programming concept is complete without a cute animal mascot.";
+
+  res.render('pages/index', {
+    mascots: mascots,
+    tagline: tagline
+  });
 });
 
 // about page
